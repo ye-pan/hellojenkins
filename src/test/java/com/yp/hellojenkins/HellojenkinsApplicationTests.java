f46e681e7800db569d128859bd3caf45a8c9790b
yepan <yepanknow@qq.com>
2019-10-27 20:04:05 +0800

    初始化项目

---
+++ src/test/java/com/yp/hellojenkins/HellojenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yp.hellojenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellojenkinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
